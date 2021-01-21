9306bb7a0390e76bbde8dc892bb3f380a1abbdd8
Aiden P <50061154+PrecurseAP@users.noreply.github.com>
2021-01-20 23:15:11 -0500

    minor changes, removing old code
    yep i made these changes 2 months ago and never commited

--- src/drive/drive.h
+++ src/drive/drive.h
@@ -3,7 +3,7 @@
 
 #include "vex.h"
 
-extern void _drive(void);
+extern void driveTheDamnRobot(void);
 extern void resetGyro(void);
 extern void stopAllDrive(brakeType);
 extern void spinMotors(int, int, int, int);
@@ -13,12 +13,8 @@ extern float logDrive_shallowish(float);
 extern float logDrive_steepish(float);
 extern float logDrive_steep(float);
 
-extern float goalAngle, angleIntegral, angleError;
-extern float normalizer, angleDerivative, previousError;
-extern float kP, kI, kD;                      
+extern float normalizer;
 extern float mSpd[4];
-extern float turnValue;
-extern bool autoTurn;
 extern double maxAxis, maxOutput;
 extern float gyroAngle;
 extern int joyX, joyY;
