189f3ccf9e052e45577b051108cb89a051f5201e
PrecurseAP <50061154+PrecurseAP@users.noreply.github.com>
2021-11-18 18:49:36 -0500

    finish the pure pursuit algorithm (almost)
    i just need the rate limiter, feedforward and feedback control i also need to make a simulation and then tune the ka kv and kp variables :)))))))))) the pain is unbearable :))))

--- src/pure-pursuit.cpp
+++ src/pure-pursuit.cpp
@@ -1,6 +1,11 @@
 #include "pure-pursuit.h"
 #include "vex.h"
 
+template <typename G> 
+int sgn2(G val) { //SIGNUM
+    return (G(0) < val) - (val < G(0));
+}
+
 /* declared in header file, this is the point data structure
 struct Point {
   float x = 0;
@@ -91,11 +96,11 @@ std::vector<Point> smoother(std::vector<Point> points, float b, float tolerance)
 std::vector<Point> calculateDistances(std::vector<Point> path) {
   std::vector<Point> newPath(path);
   
-  newPath[i].distanceOnPath = 0;
+  newPath[0].distanceOnPath = 0;
   
   
   for(int i = 1; i < path.size(); i++) {
-    newPath[i].distanceOnPath = newPath(i-1) + sqrt((newPath[i].x - newPath[i-1].x)*(newPath[i].x - newPath[i-1].x)
+    newPath[i].distanceOnPath = newPath[i-1].distanceOnPath + sqrt((newPath[i].x - newPath[i-1].x)*(newPath[i].x - newPath[i-1].x)
                                     + (newPath[i].y - newPath[i-1].y)*(newPath[i].y - newPath[i-1].y));
   }
   return newPath;
@@ -107,18 +112,18 @@ std::vector<Point> calculateDistances(std::vector<Point> path) {
 std::vector<Point> calculateCurvatures(std::vector<Point> path) {
   std::vector<Point> newPath(path);
   
-  x1 = newPath[i].x;
-  y1 = newPath[i].y;
-  x2 = newPath[i-1].x;
-  y2 = newPath[i-1].y;
-  x3 = newPath[i+1].x;
-  y3 = newPath[i+1].y;
-  
-  if (x1 == x2) {
+  for (int i = 1; i < path.size()-1; i++) {
+    float x1 = newPath[i].x;
+    float y1 = newPath[i].y;
+    float x2 = newPath[i-1].x;
+    float y2 = newPath[i-1].y;
+    float x3 = newPath[i+1].x;
+    float y3 = newPath[i+1].y;
+
+      if (x1 == x2) {
     x1 += 0.001; 
   }
-  
-  for (int i = 1; i < path.size()-1; i++) {
+    
     float k1 = 0.5 * (x1*x1 + y1*y1 - x2*x2 - y2*y2) / (x1-x2);
     float k2 = (y1 - y2) / (x1 - x2);
     float b = 0.5 * (x2*x2 - 2*x2*k1 + y2*y2 - x3*x3 + 2*x3*k1 - y3*y3) / (x3*k2 - y3 + y2 - x2*k2);
@@ -130,10 +135,139 @@ std::vector<Point> calculateCurvatures(std::vector<Point> path) {
   return newPath;
 }
 
-std::vector<Point> calculateVelocities(std::vector<Point> path, float pathMaxVel, float k) {
+std::vector<Point> calculateVelocities(std::vector<Point> path, float pathMaxVel, float k, float a) {
   std::vector<Point> newPath(path);
   
+  newPath[0].targetVelocity = pathMaxVel;
+  newPath[newPath.size()-1].targetVelocity = 0;
+
+  for(int i = 1; i < newPath.size()-1; i++) {
+    newPath[i].targetVelocity = std::min(pathMaxVel, k/newPath[i].curvature);
+  }
+
+  for(int i = newPath.size()-2; i >= 0; i--) {
+    float dis = sqrt((newPath[i].x - newPath[i+1].x)*(newPath[i].x - newPath[i+1].x)
+                + (newPath[i].y - newPath[i+1].y)*(newPath[i].y - newPath[i+1].y));
+    newPath[i].targetVelocity = std::min(newPath[i].targetVelocity, 
+                                (float)sqrt((newPath[i+1].targetVelocity)*(newPath[i+1].targetVelocity) + 2.0*a*dis));
+  }
+
   return newPath;
+}
+
+struct purePursuitData {
+  int closestPoint;
+  int prevClosestPoint = 0;
+  Point lookaheadPoint = Point(0,0);
+  float prevLookahead;
+
+  purePursuitData(Point WAP) {
+    closestPoint = 0;
+    prevClosestPoint = 0;
+    lookaheadPoint = WAP;
+    prevLookahead = 0.0;
+  }
+
+};
+float robotX = 0; //placeholders for when we actually have odometry.
+float robotY = 0;
+float robotTheta = 0;
+float robotTrackWidth = 14;
+
+float distanceFormula(Point a, Point b) {
+  return sqrt((b.x-a.x)*(b.x-a.x) + (b.y-a.y)*(b.y-a.y));
+}
+
+int purePursuit(std::vector<Point> path, float lookahead) {
+  purePursuitData data = purePursuitData(path[0]);
+
+  int start_time = Brain.timer(msec);
+  int prevTime = 0;
+
+  bool done = false;
+  while(!done) {
+    int currentTime = Brain.timer(msec);
+    int delta_time = currentTime - prevTime;
+    prevTime = currentTime;
+
+    //find closest point
+    for (int i = data.prevClosestPoint, least = 0; i < path.size(); i++) {
+      float distanceToPoint = distanceFormula(Point(robotX, robotY), path[i]);
+      if (distanceToPoint < distanceFormula(Point(robotX, robotY), path[least])) {
+        data.prevClosestPoint = i; //dont need prev variable because it is not need, closestPoint is preserved
+        least = i;
+      }
+      if(i == path.size()-1) {
+        data.closestPoint = least;
+      }
+    }
+
+    //calculate lookahead point
+    for (int i = data.prevLookahead; i < path.size()-1; i++) {
+      float dx = path[i+1].x - path[i].x;
+      float dy = path[i+1].y - path[i].y;
+
+      float fx = path[i].x - robotX;
+      float fy = path[i].y - robotY;
+
+      float a = dx*dy + dx*dy;
+      float b = 2.0 * ((fx*dx) + (fy*dy));
+      float c = (fx*fy + fx*fy) - lookahead*lookahead;
+      float discriminant = b*b - 4*a*c;
+
+      if (discriminant < 0) {
+        //no intersection
+      } else {
+        discriminant = sqrt(discriminant);
+        
+        float t1 = (-b - discriminant) / (2*a);
+        float t2 = (-b + discriminant) / (2*a);
+
+        if (t1 >= 0 && t1 <= 1) {
+          float intersectionX = path[i].x + (t1*dx);
+          float intersectionY = path[i].y + (t1*dy);
+
+          float fracIndex = i + t1;
+
+          if (fracIndex > data.prevLookahead) {
+            data.lookaheadPoint = Point(intersectionX, intersectionY);
+            data.prevLookahead = fracIndex;
+          }
+        }
+        if (t2 >= 0 && t2 <= 1) {
+          float intersectionX = path[i].x + (t2*dx);
+          float intersectionY = path[i].y + (t2*dy);
+
+          float fracIndex = i + t2;
+
+          if (fracIndex > data.prevLookahead) {
+            data.lookaheadPoint = Point(intersectionX, intersectionY);
+            data.prevLookahead = fracIndex;
+          }
+        }
+      }
+      //otherwise no intersection
+    }
+
+    float xFromRobotToLookahead = fabs((-tan(robotTheta)*data.lookaheadPoint.x)
+                                        + data.lookaheadPoint.y 
+                                        + (tan(robotTheta)*robotX - robotY))
+                                        / sqrt((-tan(robotTheta))*(-tan(robotTheta)) + 1);
+    float lookaheadCurvature = (2*xFromRobotToLookahead)/ (lookahead*lookahead);
+    float side = sgn2(sin(robotTheta)*(data.lookaheadPoint.x - robotX) 
+                      - cos(robotTheta)*(data.lookaheadPoint.y - robotY));
+
+    float signedCurvature = lookaheadCurvature * side; //that took a while
+
+    //begin wheel velocity calculation
+    float targetLeftVelocity = path[data.closestPoint].targetVelocity * ((2 + signedCurvature*robotTrackWidth) / 2);
+    float targetRightVelocity = path[data.closestPoint].targetVelocity - ((2 + signedCurvature*robotTrackWidth) / 2);
+
+
+
+    wait(20, msec);
+  }
+  return 0;
 }
 
 //draws a path to the brain screen using circles as nodes. nice for debugging.
