c207e10043101626fd2eca70517de4cd212415b3
Aiden P <50061154+PrecurseAP@users.noreply.github.com>
2021-11-18 10:00:51 -0500

    add distance, curvature, and begin velocity

--- src/pure-pursuit.cpp
+++ src/pure-pursuit.cpp
@@ -85,6 +85,56 @@ std::vector<Point> smoother(std::vector<Point> points, float b, float tolerance)
   return newPath;
 }
 
+/**
+* this is the function that accepts a vector of points (should be ouput from smoother) and adds the distance between them
+*/
+std::vector<Point> calculateDistances(std::vector<Point> path) {
+  std::vector<Point> newPath(path);
+  
+  newPath[i].distanceOnPath = 0;
+  
+  
+  for(int i = 1; i < path.size(); i++) {
+    newPath[i].distanceOnPath = newPath(i-1) + sqrt((newPath[i].x - newPath[i-1].x)*(newPath[i].x - newPath[i-1].x)
+                                    + (newPath[i].y - newPath[i-1].y)*(newPath[i].y - newPath[i-1].y));
+  }
+  return newPath;
+}
+
+/**
+* This is the function that calculates the curvature at each point, input should come from calculateDistances
+*/
+std::vector<Point> calculateCurvatures(std::vector<Point> path) {
+  std::vector<Point> newPath(path);
+  
+  x1 = newPath[i].x;
+  y1 = newPath[i].y;
+  x2 = newPath[i-1].x;
+  y2 = newPath[i-1].y;
+  x3 = newPath[i+1].x;
+  y3 = newPath[i+1].y;
+  
+  if (x1 == x2) {
+    x1 += 0.001; 
+  }
+  
+  for (int i = 1; i < path.size()-1; i++) {
+    float k1 = 0.5 * (x1*x1 + y1*y1 - x2*x2 - y2*y2) / (x1-x2);
+    float k2 = (y1 - y2) / (x1 - x2);
+    float b = 0.5 * (x2*x2 - 2*x2*k1 + y2*y2 - x3*x3 + 2*x3*k1 - y3*y3) / (x3*k2 - y3 + y2 - x2*k2);
+    float a = k1 - k2*b;
+    float r = sqrt( (x1-a)*(x1-a) + (y1-b)*(y1-b) );
+    float c = 1/r;
+    newPath[i].curvature = c; 
+  }
+  return newPath;
+}
+
+std::vector<Point> calculateVelocities(std::vector<Point> path, float pathMaxVel, float k) {
+  std::vector<Point> newPath(path);
+  
+  return newPath;
+}
 
 //draws a path to the brain screen using circles as nodes. nice for debugging.
 void drawOnBrain(std::vector<Point> points, vex::color Color, int radius) {
